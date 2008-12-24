8266c2d1059686ee0f3f213dc2b4809173d41af1
Hans Engel <engel@engel.uk.to>
2008-12-24 15:05:27 -0700

    various structural updates

--- lib/snippet.rb
+++ lib/snippet.rb
@@ -1,18 +1,31 @@
-require 'optparse'
 require 'grit'
 
 class Snippet
   def initialize(args)
-    run_command args.shift, args
+    $args = args
+    $basedir = File.join ENV['HOME'], '.snippet'
+    run_command
   end
   
-  def run_command(command, args)
+  def run_command
+    command = $args.shift
+    init_repo command
     put_help if command.nil?
     file = File.join File.dirname(__FILE__), 'commands', command + '.rb'
     if File.exists? file
       require file
     else
       put_help
+    end
+  end
+
+  def init_repo(command)
+    begin
+      $repo = Grit::Repo.new $basedir unless command == 'help'
+    rescue
+      Dir.mkdir $basedir rescue nil
+      `cd #{$basedir}; git init`
+      $repo = Grit::Repo.new $basedir unless command == 'help'
     end
   end
 
